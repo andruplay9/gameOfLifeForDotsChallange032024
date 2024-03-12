29960ecdce214ba283264ccd75e8817838b003f2
andruplay9 <andrzejszablewski13@gmail.com>
2024-03-12 23:28:15 +0100

    additional data

---
+++ Assets/ECS/GameOfLifeSystemGroupSystem.cs
@@ -0,0 +1,46 @@
+﻿using Unity.Entities;
+
+namespace GameOfLife.ECS
+{
+    [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+    public partial class GameOfLifeSystemGroupSystem : ComponentSystemGroup
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+        }
+
+        protected override void OnUpdate()
+        {
+            base.OnUpdate();
+        }
+    }
+    [UpdateInGroup(typeof(GameOfLifeSystemGroupSystem))]
+    public partial class GameOfLifeSimulationSystemGroupSystem : ComponentSystemGroup
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            RequireForUpdate<RunSimulationTag>();
+        }
+
+        protected override void OnUpdate()
+        {
+            base.OnUpdate();
+        }
+    }
+    [UpdateInGroup(typeof(GameOfLifeSystemGroupSystem))]
+    [UpdateBefore(typeof(GameOfLifeSimulationSystemGroupSystem))]
+    public partial class GameOfLifeCrationAndIOSystemGroupSystem : ComponentSystemGroup
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+        }
+
+        protected override void OnUpdate()
+        {
+            base.OnUpdate();
+        }
+    }
+}
